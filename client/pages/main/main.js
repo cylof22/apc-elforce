b8c807bd232ba242fbd9bc5388c88af7b99342da
cylof22 <cylof22@gmail.com>
2018-05-30 12:47:15 +0800

    add product main page

--- client/pages/main/main.js
+++ client/pages/main/main.js
@@ -1,3 +1,56 @@
 Page({
+    data: {
+        // 图片列表
+        productList: [{
+          id: 1,
+          image: 'http://localhost:5000/preview/outputs/BtoA_cezanne2photo_256_DSC_0119.jpg',
+          name: '商品1',
+        }, {
+          id: 2,
+          image: 'http://localhost:5000/preview/outputs/7HL_8671.jpg',
+          name: '商品2',
+        }, {
+          id: 3,
+          image: 'http://localhost:5000/preview/outputs/7Inch_4.jpg',
+          name: '商品3',
+        }, {
+          id: 4,
+          image: 'http://localhost:5000/preview/outputs/BtoA_cezanne2photo_256_DSC_0126.jpg',
+          name: '商品4',
+        }, {
+          id: 5,
+          image: 'http://localhost:5000/preview/outputs/BtoA_cezanne2photo_256_DSC_7442.jpg',
+          name: '商品5',
+        },
+        {
+          id: 6,
+          image: 'http://localhost:5000/preview/outputs/DSC_8128.jpg',
+          name: '商品6',
+        }], // 商品列表
     
+        // 是否显示loading
+        showLoading: false,
+    
+        // loading提示语
+        loadingMessage: '',
+      },
+
+      onLoad() {
+        this.getProducts();
+      },
+
+      // request all the products
+      getProducts() {
+
+      },
+
+      // 预览图片
+      previewProduct(event) {
+        var imgURL = event.target.dataset.src; 
+        console.log(imgURL)
+        wx.previewImage({  
+            current: imgURL, // 当前显示图片的http链接  
+            urls: [imgURL]// 需要预览的图片http链接列表  
+        })  
+      }
 })
