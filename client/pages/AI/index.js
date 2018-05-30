8b0303979c5ad701ebcc5213e2a071a48dbe7949
cylof22 <cylof22@gmail.com>
2018-05-30 13:31:58 +0800

    add save and share function

--- client/pages/AI/index.js
+++ client/pages/AI/index.js
@@ -209,7 +209,37 @@ Page({
     },
 
     saveAndShare() {
-        // 
+        // Todo: How to add qrcode to the image, right corner?
+        wx.downloadFile({
+            url: this.data.previewImgURL,
+            success:function (res) {
+                console.log(res);
+                //图片保存到本地
+                wx.saveImageToPhotosAlbum({
+                    filePath: res.tempFilePath,
+                    success:function (data) {
+                        onsole.log(data);
+                    },
+                    fail:function (err) {
+                        console.log(err);
+                        if (err.errMsg === "saveImageToPhotosAlbum:fail auth deny") {
+                            console.log("用户一开始拒绝了，我们想再次发起授权")
+                            console.log('打开设置窗口')
+                            wx.openSetting({
+                                success(settingdata) {
+                                    console.log(settingdata)
+                                    if (settingdata.authSetting['scope.writePhotosAlbum']) {
+                                        console.log('获取权限成功，给出再次点击图片保存到相册的提示。')
+                                    } else {
+                                        console.log('获取权限失败，给出不给权限就无法正常使用的提示')
+                                    }
+                                }
+                            })  
+                        }
+                    }
+                })
+            }
+        })     
     },
     /**
      * 统一封装选择图片和上传图片的 API
