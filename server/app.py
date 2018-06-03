84a93bf28c207fb928657ae85a4d21e99de00b4d
cylof22 <cylof22@gmail.com>
2018-06-03 13:42:26 +0800

    change http to https

--- server/app.py
+++ server/app.py
@@ -212,5 +212,5 @@ if __name__ == '__main__':
     MODEL_DIR = options.modeldir
     CHECKPOINT_DIR = options.checkpointdir
 
-    ssl = ('./certification/server.crt', './certification/server.key')
-    app.run(host=options.host,port=int(options.port))
+    ssl = ('./certification/server.pem', './certification/server.key')
+    app.run(host=options.host,port=int(options.port), ssl_context=ssl)
